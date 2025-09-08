0c779f721d9040fb6c1d53cef10b7fcec56cabd5
prachis-nd <prachi.singh@netradyne.com>
2025-09-08 18:04:23 +0530

    added sample json

--- scripts/ingestData.py
+++ scripts/ingestData.py
@@ -1,6 +1,9 @@
+import os
 from app.service.dataIngestionService.processor import Processor
 
 if __name__ == "__main__":
-    file_path = "/Users/prachisingh/Downloads/playlist[76][36][48][6][49][41][28][85][7][90][20][31][56][54].json"
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "sample.json")
+    
     processor = Processor(file_path)
     processor.process()
